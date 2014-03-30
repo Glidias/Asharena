49d455056683b1e5893b66ddc571a7860383030f
Glidias <glidias@hotmail.om>
2014-03-30 11:03:05 +0800

    added Health Component to AggroMemNode to keep track of ALIVE nodes!

--- src/arena/systems/enemy/AggroMemNode.hx
+++ src/arena/systems/enemy/AggroMemNode.hx
@@ -2,6 +2,7 @@ package arena.systems.enemy;
 import arena.components.char.AggroMem;
 import ash.core.Node.Node;
 import components.Ellipsoid;
+import components.Health;
 import components.Pos;
 import components.Rot;
 
@@ -15,7 +16,7 @@ class AggroMemNode extends Node<AggroMemNode>
 	public var pos:Pos;
 	public var rot:Rot;
 	public var size:Ellipsoid;
-	
+	public var health:Health;
 	
 	public function new() 
 	{
