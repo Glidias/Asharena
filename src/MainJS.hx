5cf7cdd65c16ab550ec0ca1614445c00b277ea86
Glidias <glidias@hotmail.com>
2018-04-11 16:25:10 +0800

    mainjs (and other lib) export file

---
+++ src/MainJS.hx
@@ -0,0 +1,46 @@
+package;
+
+#if js
+import js.Lib;
+#end
+
+
+///*
+import arena.pathfinding.GKDijkstra;
+import arena.pathfinding.GKGraph;
+import arena.pathfinding.GKGrapher;
+import arena.pathfinding.GKNode;
+//*/
+
+/*
+import de.polygonal.ds.Graph;
+*/
+
+///* 
+import systems.collisions.EllipsoidCollider;
+//*/
+
+///* 
+import altern.collisions.CollisionBoundNode;
+//*/
+
+
+/* Ash-framework integration
+import systems.collisions.EllipsoidColliderSystem;
+*/
+
+
+
+/**
+ * Package for deploying to JS and other platforms for third-party use (besides Flash)
+ * @author Glidias
+ */
+class MainJS
+{
+	
+	static function main() 
+	{
+		
+	}
+	
+}
